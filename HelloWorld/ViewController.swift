5afa70c3a57500d74ec5347c178736ec211cf65e
Tsuchiya-Ringtech <tsuchiya.kazunori@ring-tech.jp>
2023-06-09 10:07:46 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Kazunori Tsuchiya on 2023/06/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
